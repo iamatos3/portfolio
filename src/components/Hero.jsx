80e4825213562ec59949a647baf4922800cf15d2
iamatos3 <matosian93@gmail.com>
2023-09-06 14:14:40 -0400

    Created the Hero component w/ functionality and styling

---
+++ src/components/Hero.jsx
@@ -0,0 +1,38 @@
+import heroImg from '../assets/hero.svg';
+import { FaGithubSquare, FaLinkedin, FaInstagramSquare } from 'react-icons/fa';
+
+const Hero = () => {
+  return (
+    <div className='bg-emerald-100 py-24'>
+      <div className='mx-auto max-w-7xl px-8 grid md:grid-cols-2 items-center gap-8'>
+        <article>
+          <h1 className='text-7xl font-bold tracking-wider'>
+            Ian Matos
+          </h1>
+          <p className='mt-4 text-3xl text-slate-700 capitalize tracking-wide'>
+            Full-Stack Software Engineer
+          </p>
+          <p className='mt-2 text-lg text-slate-700 capitalize tracking-wide'>
+            Turning ideas into interactive reality
+          </p>
+          <div className='flex gap-x-4 mt-4'>
+            <a href='#'>
+              <FaGithubSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+            </a>
+            <a href='#'>
+              <FaLinkedin className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+            </a>
+            <a href='#'>
+              <FaInstagramSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+            </a>
+          </div>
+        </article>
+        <article className='hidden md:block'>
+          <img src={heroImg} className='h-80 lg:h-96' />
+        </article>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
