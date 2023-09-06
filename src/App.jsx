54920c2b5541096d34c586292caa2d461f899e50
iamatos3 <matosian93@gmail.com>
2023-09-06 13:53:02 -0400

    Created the Navbar component of the project
    - installed Tailwind and React Icons
    - Removed the project-assets folder
    - Moved info from that folder to the assets folder under src

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">Ian Matos</h1>
+    <>
+      <Navbar />
+    </>
   );
 };
 
