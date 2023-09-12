ee1b53f978461ea941240dc6b997afbb99cc8203
iamatos3 <matosian93@gmail.com>
2023-09-12 10:58:50 -0400

    Updated Hero component to redirect with button options to specific sites
    - additionally added download button option for resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,9 +23,6 @@ const Hero = () => {
             <a href='https://www.linkedin.com/in/iamatos/' title='LinkedIn' target="_blank">
               <FaLinkedin className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
             </a>
-            <a href='https://www.instagram.com/iamatos3/' title='Instagram' target="_blank">
-              <FaInstagramSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
-            </a>
             <a href='#' title='Download Resume' download>
               <BsDownload className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
             </a>
